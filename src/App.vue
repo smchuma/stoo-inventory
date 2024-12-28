f6bce3460e71d656c759eb9f59b8f160cfe7f0fc
Samora Mchuma <laurentsamora6@gmail.com>
2024-12-29 02:37:04 +0300

    initialized the project and cleaned the folders

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Sidebar from "./components/Sidebar/Sidebar.vue";
+import Navbar from "./components/Navbar/Navbar.vue";
+</script>
+
+<template>
+  <Sidebar />
+  <Navbar />
+</template>
---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="">Sidebar</h1>
+</template>
